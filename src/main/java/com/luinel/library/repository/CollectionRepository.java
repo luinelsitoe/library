6187220ef0d328afce8390705e465e46ec7508cf
luinelsitoe <luinelsitoe@gmail.com>
2025-08-13 14:28:51 +0200

    Metodos de filtragem

--- src/main/java/com/luinel/library/repository/CollectionRepository.java
+++ src/main/java/com/luinel/library/repository/CollectionRepository.java
@@ -1,11 +1,21 @@
 package com.luinel.library.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.luinel.library.model.Collection;
+import com.luinel.library.model.BookCollection;
 
 @Repository
-public interface CollectionRepository extends JpaRepository<Collection, Long> {
+public interface CollectionRepository extends JpaRepository<BookCollection, Long> {
+  List<BookCollection> findAllByUserId(Long userId);
+
+  Optional<BookCollection> findByIdAndUserId(Long collectionId, Long userId);
+
+  List<BookCollection> findByNameContainingAndUserId(String name, Long userId);
+
+  void deleteByIdAndUserId(Long collectionId, Long userId);
 
 }
