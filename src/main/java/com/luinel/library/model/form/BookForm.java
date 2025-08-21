b0dde10922f9a5878c09c2dd33b9788f4c956c2c
luinelsitoe <luinelsitoe@gmail.com>
2025-08-21 09:02:45 +0200

    Construtores

--- src/main/java/com/luinel/library/model/form/BookForm.java
+++ src/main/java/com/luinel/library/model/form/BookForm.java
@@ -5,9 +5,13 @@ import com.luinel.library.model.enums.Genre;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookForm {
   @NotBlank(message = "Preencha o titulo")
   private String title;
--- src/main/java/com/luinel/library/model/form/UpdateUserForm.java
+++ src/main/java/com/luinel/library/model/form/UpdateUserForm.java
@@ -1,9 +1,13 @@
 package com.luinel.library.model.form;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateUserForm {
   @NotBlank(message = "Preencha o nome")
   private String name;
